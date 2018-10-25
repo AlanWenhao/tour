767ee3e262d9df9a68c478c7a07fa0d499595178
zhangwenhao <yuerang.zwh@raycloud.com>
2018-10-25 20:04:30 +0800

    header component

--- README.md
+++ README.md
@@ -13,3 +13,12 @@ $ npm run dev
 ```
 
 ## what I use
+
+## Repos that I want to try
+
+[react-motion](https://github.com/chenglou/react-motion)  
+[react-magic](https://github.com/lit-forest/react-magic)  
+[popmotion](https://github.com/Popmotion/popmotion)  
+
+## Links
+![关于react动画  --知乎](https://zhuanlan.zhihu.com/p/28536964)
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,8 +5,14 @@ class Header extends Component {
     render() {
         return (
             <div className="c-header">
-                <Link to="/">主页</Link>
-                <Link to="/category">分类页</Link>
+                <div className="c-header__main">
+                    <h1 className="c-header__title">tour <span>poster</span></h1>
+                    <div>
+                        <Link className="c-header__link" to="/">home</Link>
+                        <Link className="c-header__link" to="/category">类目</Link>
+                        <Link className="c-header__link" to="/contact">联系</Link>
+                    </div>
+                </div>
             </div>
         );
     }
