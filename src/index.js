4260bfe58bed98204bdd02927b2fea3ce5e0d543
zhangwenhao <yuerang.zwh@raycloud.com>
2018-11-07 14:22:32 +0800

    change react-router-redux version to next

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
-<<<<<<< HEAD
-// import { HashRouter as Router } from 'react-router-dom';
-import { ConnectedRouter } from 'react-router-redux';
-=======
 import { ConnectedRouter } from 'react-router-redux';
 // import '@/assets/sass/main.less';
->>>>>>> 41c78f4c191268ff65b0cec4c0667a6d6dd82336
 import '@/assets/sass/index.scss';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -20,9 +15,5 @@ ReactDom.render(
         <ConnectedRouter history={history}>
             <App />
         </ConnectedRouter>
-<<<<<<< HEAD
-    </Provider>, document.querySelector('#root'),
-=======
     </Provider>, window.root,
->>>>>>> 41c78f4c191268ff65b0cec4c0667a6d6dd82336
 );
