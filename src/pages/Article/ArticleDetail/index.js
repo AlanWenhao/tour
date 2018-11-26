861d530292c8fea3d450cbc4214bb6fdb921323d
zhangwenhao <yuerang.zwh@raycloud.com>
2018-11-26 09:53:57 +0800

    store

---
+++ src/pages/Article/ArticleDetail/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class ArticleDetail extends Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    render() {
+        return (
+            <div className="c-article-detail">
+                <div>这里是文章详情</div>
+                <dir className="c-article-detail__action">
+
+                </dir>
+            </div>
+        );
+    }
+}
+
+export default ArticleDetail;
