090e0bb5b4a4bc00e624a2e5f3f980f75fa578d1
AlanWenhao <13454782783@163.com>
2018-12-05 00:01:39 +0800

    store

--- src/pages/Mine/index.js
+++ src/pages/Mine/index.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import { Tabs } from 'antd';
+
+function callback(key) {
+    console.log(key);
+}
 
 class Mine extends Component {
     render() {
+        const { TabPane } = Tabs;
         return (
-            <div>MINE</div>
+            <div className="t-mine">
+                <div className="t-mine__header">
+                    <div className="t-mine__avatar">
+                        <img src="" alt=""/>
+                    </div>
+                    <h4 className="t-mine__username">Alan</h4>
+                </div>
+                <Tabs defaultActiveKey="1" onChange={callback}>
+                    <TabPane tab="Tab 1" key="1">Content of Tab Pane 1</TabPane>
+                    <TabPane tab="Tab 2" key="2">Content of Tab Pane 2</TabPane>
+                    <TabPane tab="Tab 3" key="3">Content of Tab Pane 3</TabPane>
+                </Tabs>
+            </div>
         );
     }
 }
